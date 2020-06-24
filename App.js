25e651d36420c0dccd757d48666bb5f606cb1fee
valAizcorbe <valeria.aizcorbe@hotmail.com>
2020-06-23 22:42:34 -0600

    small app

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [title, setTitle] = useState("This is Val App jiji");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{title}</Text>
+      <Text style={styles.funText}> This is a fun text</Text>
+      <Button
+        title="Change Text"
+        onPress={() => setTitle("Text changed! juju")}
+      />
     </View>
   );
 }
@@ -12,8 +19,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  funText: {
+    backgroundColor: "red",
+    fontWeight: "bold",
+    color: "#fff",
+    height: 50,
+    width: 150,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
